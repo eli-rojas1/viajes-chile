7ed32d00d0bf13e3e4acd88985349474ea7411b4
Elisa Rojas <elisa.rojas.reyes@gmail.com>
2022-04-12 00:14:32 -0400

    prueba1

---
+++ assets/js/buttons.js
@@ -0,0 +1,9 @@
+$("#button1").click( function() {
+    $("#info1").toggle('slow')
+});
+$("#button2").click( function() {
+    $("#info2").toggle('show')
+});
+$("#button3").click( function() {
+    $("#info3").toggle('show')
+});
